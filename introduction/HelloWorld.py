e8877a920b1d933e4052cf46156461a5221efefd
ed7543 <ed7543@student.uni-lj.si>
2024-11-09 16:47:27 +0100

    first games

---
+++ introduction/HelloWorld.py
@@ -0,0 +1,17 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+#this function returns object for the window
+DISPLAYSURF = pygame.display.set_mode((400, 300)) #this is the width and height for the window
+pygame.display.set_caption('Hello World')
+
+while True: #main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
+#setting the title of the game
+#caption is basically the title
